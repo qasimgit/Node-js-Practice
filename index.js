f6b767ace38869fa2a979a0151b88b09e970e15c
MUHAMMAD QASIM QADRI <qasimpkkhi@gmail.com>
2021-12-14 00:00:54 +0500

    express added

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("node js pracitce");
+const express = require("express");
+const http = require("http");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, (err) => {
+  if (err) console.log("error in starting server");
+  return console.log("PORT running on ", PORT);
+});
